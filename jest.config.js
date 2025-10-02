763f1e494a621512fc4fcb5b1dbeaa2325dfdbaf
Michael Lavigna <mlavigna@it.iliad.com>
2025-10-02 11:24:11 +0200

    feat: updated deps and added jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testTimeout: 10000,
+  verbose: true
+};
